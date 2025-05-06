f339dde6dd9596636a437056253eb07e374eab8c
Nadezhda Kotegova <kotegova@gnivc.ru>
2025-05-06 23:17:07 +0300

    fix: code styles

--- src/test/java/ru/yandex/practicum/filmorate/dal/JdbcFilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/JdbcFilmRepositoryTest.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
